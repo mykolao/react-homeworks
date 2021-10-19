936e5b8425a55c3f11bef262ff25c55baaa7d5c2
mykolao <mykolaoleskiv@gmail.com>
2021-10-19 23:36:23 +0300

    section styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import HW1 from "./homework-1/HW1";
+import HW2 from "./homework-2/HW2";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HW1 />
+    <div>
+      <HW1 />
+      <HW2 />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
