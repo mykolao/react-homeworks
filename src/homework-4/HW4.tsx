133f49e04d5ce58ca130366e18bbaebea5c3ca7c
mykolao <mykolaoleskiv@gmail.com>
2021-11-02 16:52:57 +0200

    uppercase

--- src/homework-4/HW4.tsx
+++ src/homework-4/HW4.tsx
@@ -38,16 +38,16 @@ function HW4() {
 
         {/*----------------------------------------------------*/}
 
-        <SuperButton>default</SuperButton>
+        <SuperButton>Default</SuperButton>
 
         <SuperButton
           red // пропсу с булевым значением не обязательно указывать true
           onClick={showAlert}
         >
-          delete {/*// название кнопки попадёт в children*/}
+          Delete {/*// название кнопки попадёт в children*/}
         </SuperButton>
 
-        <SuperButton disabled>disabled</SuperButton>
+        <SuperButton disabled>Disabled</SuperButton>
 
         {/*----------------------------------------------------*/}
 
