4a969e5f4ebfd2571f6072da4ea00a8ea19ce112
mykolao <mykolaoleskiv@gmail.com>
2021-11-02 16:15:28 +0200

    SuperInputText styling

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import HW1 from "./homework-1/HW1";
 import HW2 from "./homework-2/HW2";
 import HW3 from "./homework-3/HW3";
+import HW4 from "./homework-4/HW4";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.render(
       <HW1 />
       <HW2 />
       <HW3 />
+      <HW4 />
     </div>
   </React.StrictMode>,
   document.getElementById("root")
