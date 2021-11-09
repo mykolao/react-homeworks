7cb936f10aa38905a4d990f203eb547124671f30
mykolao <mykolaoleskiv@gmail.com>
2021-11-10 01:13:16 +0200

    404 styling

--- src/homework-5/pages/Error404.tsx
+++ src/homework-5/pages/Error404.tsx
@@ -1,9 +1,11 @@
+import css from "./Error404.module.css";
+
 function Error404() {
   return (
-    <div>
-      <div>404</div>
-      <div>Page not found!</div>
-      <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+    <div className={css.error}>
+      <div className={css.number}>404</div>
+      <div className={css.message}>Page not found!</div>
+      <div className={css.ascii}>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
     </div>
   );
 }
