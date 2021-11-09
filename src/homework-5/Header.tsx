f988e497a0bca8a1304b048080be1ccfa6b125c0
mykolao <mykolaoleskiv@gmail.com>
2021-11-10 01:03:26 +0200

    nav animated

--- src/homework-5/Header.tsx
+++ src/homework-5/Header.tsx
@@ -1,20 +1,52 @@
 import { NavLink } from "react-router-dom";
 import { PATH } from "./Routes";
 import css from "./Header.module.css";
+import { useState } from "react";
 
 function Header() {
+  const [navHider, setNavHider] = useState<"<" | ">">(">");
+
+  const [navState, setNavState] = useState<
+    "visible" | "hidden"
+  >("hidden");
+
+  const onHoverHandler = () => {
+    setNavHider(navHider === ">" ? "<" : ">");
+    setNavState(navState === "hidden" ? "visible" : "hidden");
+  };
   return (
-    <nav className={css.header}>
-      <NavLink className={css.nav_link} to={PATH.PRE_JUNIOR}>
-        Pre Junior
-      </NavLink>
-      <NavLink className={css.nav_link} to={PATH.JUNIOR}>
-        Junior
-      </NavLink>
-      <NavLink className={css.nav_link} to={PATH.JUNIOR_PLUS}>
-        Junior+{" "}
-      </NavLink>
-    </nav>
+    <div
+      className={css.header}
+      onMouseEnter={onHoverHandler}
+      onMouseLeave={onHoverHandler}
+    >
+      <nav className={`${css.nav} ${css[navState]}`}>
+        <NavLink
+          className={css.nav_link}
+          activeClassName={css.active}
+          to={PATH.PRE_JUNIOR}
+        >
+          Pre Junior
+        </NavLink>
+        <NavLink
+          className={css.nav_link}
+          activeClassName={css.active}
+          to={PATH.JUNIOR}
+        >
+          Junior
+        </NavLink>
+        <NavLink
+          className={css.nav_link}
+          activeClassName={css.active}
+          to={PATH.JUNIOR_PLUS}
+        >
+          Junior+
+        </NavLink>
+      </nav>
+      <div className={`${css.nav_hider} ${css[navState]}`}>
+        {navHider}
+      </div>
+    </div>
   );
 }
 
