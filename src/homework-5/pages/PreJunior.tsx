8d991c10c75ab7dbe1ed73b8e22e72a2c0ac414c
mykolao <mykolaoleskiv@gmail.com>
2021-11-29 01:01:45 +0200

    homework 7 init

--- src/homework-5/pages/PreJunior.tsx
+++ src/homework-5/pages/PreJunior.tsx
@@ -3,6 +3,7 @@ import HW2 from "../../homework-2/HW2";
 import HW3 from "../../homework-3/HW3";
 import HW4 from "../../homework-4/HW4";
 import HW6 from "../../homework-6/HW6";
+import HW7 from "../../homework-7/HW7";
 
 function PreJunior() {
   return (
@@ -12,6 +13,7 @@ function PreJunior() {
       <HW3 />
       <HW4 />
       <HW6 />
+      <HW7 />
     </div>
   );
 }
