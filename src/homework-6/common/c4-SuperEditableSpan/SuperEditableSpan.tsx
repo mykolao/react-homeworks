3045d5683e99f80136c4b9f59136a25b0a8241d2
mykolao <mykolaoleskiv@gmail.com>
2021-11-29 00:55:50 +0200

    functionality completed

--- src/homework-6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/homework-6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -48,21 +48,21 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = ({
   } = spanProps || {};
 
   const onEnterCallback = () => {
-    // setEditMode() // выключить editMode при нажатии Enter
+    setEditMode(false);
 
     onEnter && onEnter();
   };
   const onBlurCallback = (
     e: React.FocusEvent<HTMLInputElement>
   ) => {
-    // setEditMode() // выключить editMode при нажатии за пределами инпута
+    setEditMode(false);
 
     onBlur && onBlur(e);
   };
   const onDoubleClickCallBack = (
     e: React.MouseEvent<HTMLSpanElement, MouseEvent>
   ) => {
-    // setEditMode() // включить editMode при двойном клике
+    setEditMode(true);
 
     onDoubleClick && onDoubleClick(e);
   };
