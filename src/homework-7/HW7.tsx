83d86072f205054796609979a31bc1a2d5f050ba
mykolao <mykolaoleskiv@gmail.com>
2021-11-29 01:04:26 +0200

    basic styling & format

--- src/homework-7/HW7.tsx
+++ src/homework-7/HW7.tsx
@@ -1,41 +1,33 @@
-import React, {useState} from 'react'
-import SuperSelect from './common/c5-SuperSelect/SuperSelect'
-import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import React, { useState } from "react";
+import SuperSelect from "./common/c5-SuperSelect/SuperSelect";
+import SuperRadio from "./common/c6-SuperRadio/SuperRadio";
 
-const arr = ['x', 'y', 'z']
+const arr = ["x", "y", "z"];
 
 function HW7() {
-    const [value, onChangeOption] = useState(arr[1])
+  const [value, onChangeOption] = useState(arr[1]);
 
-    return (
-        <div>
-            <hr/>
-            homeworks 7
-
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperSelect
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
-            <div>
-                <SuperRadio
-                    name={'radio'}
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperSelect/>*/}
-            {/*<AlternativeSuperRadio/>*/}
-            <hr/>
-        </div>
-    )
+  return (
+    <div className="homework">
+      <h2 className="section_title">Homework 7</h2>
+      <div>
+        <SuperSelect
+          options={arr}
+          value={value}
+          onChangeOption={onChangeOption}
+        />
+      </div>
+      <div>
+        <SuperRadio
+          name={"radio"}
+          options={arr}
+          value={value}
+          onChangeOption={onChangeOption}
+        />
+      </div>
+      <hr />
+    </div>
+  );
 }
 
-export default HW7
+export default HW7;
