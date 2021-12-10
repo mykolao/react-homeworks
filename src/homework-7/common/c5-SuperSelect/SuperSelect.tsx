48beafb8625d3bee8ca40b4d9f945f4511911ba3
mykolao <mykolaoleskiv@gmail.com>
2021-12-10 19:42:34 +0200

    format

--- src/homework-7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/homework-7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,30 +1,38 @@
-import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import React, {
+  SelectHTMLAttributes,
+  DetailedHTMLProps,
+  ChangeEvent,
+} from "react";
 
-type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+type DefaultSelectPropsType = DetailedHTMLProps<
+  SelectHTMLAttributes<HTMLSelectElement>,
+  HTMLSelectElement
+>;
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+  options?: any[];
+  onChangeOption?: (option: any) => void;
+};
 
-const SuperSelect: React.FC<SuperSelectPropsType> = (
-    {
-        options,
-        onChange, onChangeOption,
-        ...restProps
-    }
-) => {
-    const mappedOptions: any[] = []; // map options with key
+const SuperSelect: React.FC<SuperSelectPropsType> = ({
+  options,
+  onChange,
+  onChangeOption,
+  ...restProps
+}) => {
+  const mappedOptions: any[] = []; // map options with key
 
-    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
-    }
+  const onChangeCallback = (
+    e: ChangeEvent<HTMLSelectElement>
+  ) => {
+    // onChange, onChangeOption
+  };
 
-    return (
-        <select onChange={onChangeCallback} {...restProps}>
-            {mappedOptions}
-        </select>
-    )
-}
+  return (
+    <select onChange={onChangeCallback} {...restProps}>
+      {mappedOptions}
+    </select>
+  );
+};
 
-export default SuperSelect
+export default SuperSelect;
--- src/homework-7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/homework-7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,40 +1,52 @@
-import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
+import React, {
+  ChangeEvent,
+  InputHTMLAttributes,
+  DetailedHTMLProps,
+} from "react";
 
-type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultRadioPropsType = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+  options?: any[];
+  onChangeOption?: (option: any) => void;
+};
 
-const SuperRadio: React.FC<SuperRadioPropsType> = (
-    {
-        type, name,
-        options, value,
-        onChange, onChangeOption,
-        ...restProps
-    }
-) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
-    }
+const SuperRadio: React.FC<SuperRadioPropsType> = ({
+  type,
+  name,
+  options,
+  value,
+  onChange,
+  onChangeOption,
+  ...restProps
+}) => {
+  const onChangeCallback = (
+    e: ChangeEvent<HTMLInputElement>
+  ) => {
+    // onChange, onChangeOption
+  };
 
-
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
+  const mappedOptions: any[] = options
+    ? options.map(
+        (
+          o,
+          i // map options with key
+        ) => (
+          <label key={name + "-" + i}>
             <input
-                type={'radio'}
-                // name, checked, value, onChange
+              type={"radio"}
+              // name, checked, value, onChange
             />
             {o}
-        </label>
-    )) : []
+          </label>
+        )
+      )
+    : [];
 
-    return (
-        <>
-            {mappedOptions}
-        </>
-    )
-}
+  return <>{mappedOptions}</>;
+};
 
-export default SuperRadio
+export default SuperRadio;
