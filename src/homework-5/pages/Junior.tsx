bba2f7644a461395f488d67ff0221e0e800485b3
mykolao <mykolaoleskiv@gmail.com>
2022-01-03 23:38:28 +0200

    homework 8 init

--- src/homework-5/pages/Junior.tsx
+++ src/homework-5/pages/Junior.tsx
@@ -1,5 +1,14 @@
+import HW7 from "../../homework-7/HW7";
+import HW8 from "../../homework-8/HW8";
+
 const Junior = () => {
-  return <div className="section_title">Junior Page</div>;
+  return (
+    <>
+      {/* <div className="section_title">Junior Page</div>; */}
+      <HW7 />
+      <HW8 />
+    </>
+  );
 };
 
 export default Junior;
--- src/homework-5/pages/PreJunior.tsx
+++ src/homework-5/pages/PreJunior.tsx
@@ -3,7 +3,6 @@ import HW2 from "../../homework-2/HW2";
 import HW3 from "../../homework-3/HW3";
 import HW4 from "../../homework-4/HW4";
 import HW6 from "../../homework-6/HW6";
-import HW7 from "../../homework-7/HW7";
 
 function PreJunior() {
   return (
@@ -13,7 +12,6 @@ function PreJunior() {
       <HW3 />
       <HW4 />
       <HW6 />
-      <HW7 />
     </div>
   );
 }
