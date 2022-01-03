7bc87b45e6518126934471ed39d0e57491008954
mykolao <mykolaoleskiv@gmail.com>
2022-01-04 00:41:45 +0200

    removed unused variable

--- src/homework-6/localStorage/localStorage.ts
+++ src/homework-6/localStorage/localStorage.ts
@@ -24,4 +24,4 @@ type StateType = {
 saveState<StateType>('test', {x: 'A', y: 1})
 
 // получем в переменную state объект из ячейки 'test' или дэфолтный объект если ячейка пуста
-const state: StateType = restoreState<StateType>('test', {x: '', y: 0})
+// const state: StateType = restoreState<StateType>('test', {x: '', y: 0})
