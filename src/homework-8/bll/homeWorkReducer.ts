77670a03719a9e61e11bcda0b96f7d8f0e7ff4c1
mykolao <mykolaoleskiv@gmail.com>
2022-01-03 23:49:22 +0200

    reducer init

--- src/homework-8/bll/homeWorkReducer.ts
+++ src/homework-8/bll/homeWorkReducer.ts
@@ -1,13 +1,18 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+export const homeWorkReducer = (
+  state: any,
+  action: any
+): any => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // need to fix
+      return state;
     }
-}
+    case "check": {
+      // need to fix
+      return state;
+    }
+    default:
+      return state;
+  }
+};
