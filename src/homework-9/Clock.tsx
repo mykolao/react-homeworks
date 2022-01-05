ef4f7e936ddbe5e5f201a8c279839dc42727c141
mykolao <mykolaoleskiv@gmail.com>
2022-01-05 22:19:50 +0200

    clock logic completed

--- src/homework-9/Clock.tsx
+++ src/homework-9/Clock.tsx
@@ -1,5 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import SuperButton from "../homework-4/common/c2-SuperButton/SuperButton";
+
+const formatTime = (date: Date): string => {
+  let hh = String(date.getHours());
+  let mm = String(date.getMinutes());
+  let ss = String(date.getSeconds());
+
+  hh = +hh < 10 ? `0${hh}` : hh;
+  mm = +mm < 10 ? `0${mm}` : mm;
+  ss = +ss < 10 ? `0${ss}` : ss;
+
+  return `${hh}:${mm}:${ss}`;
+};
+
+const formatDate = (date: Date): string => {
+  let yy = String(date.getFullYear());
+  let mm = String(date.getMonth() + 1);
+  let dd = String(date.getDay() + 1);
+
+  mm = +mm < 10 ? `0${mm}` : mm;
+  dd = +dd < 10 ? `0${dd}` : dd;
+
+  return `${yy}:${mm}:${dd}`;
+};
 
 function Clock() {
   const [timerId, setTimerId] = useState<number>(0);
@@ -7,25 +30,30 @@ function Clock() {
   const [show, setShow] = useState<boolean>(false);
 
   const stop = () => {
-    // stop
+    window.clearInterval(timerId);
   };
   const start = () => {
     stop();
     const id: number = window.setInterval(() => {
-      // setDate
+      setDate(new Date());
     }, 1000);
     setTimerId(id);
   };
 
   const onMouseEnter = () => {
-    // show
+    setShow(true);
   };
   const onMouseLeave = () => {
-    // close
+    setShow(false);
   };
 
-  const stringTime = "Time"; // fix with date
-  const stringDate = "Date"; // fix with date
+  const stringTime = date
+    ? formatTime(date)
+    : formatTime(new Date());
+
+  const stringDate = date
+    ? formatDate(date)
+    : formatDate(new Date());
 
   return (
     <div>
@@ -38,8 +66,8 @@ function Clock() {
 
       {show && <div>{stringDate}</div>}
 
-      <SuperButton onClick={start}>start</SuperButton>
-      <SuperButton onClick={stop}>stop</SuperButton>
+      <SuperButton onClick={start}>Start</SuperButton>
+      <SuperButton onClick={stop}>Stop</SuperButton>
     </div>
   );
 }
--- src/homework-9/HW9.tsx
+++ src/homework-9/HW9.tsx
@@ -4,7 +4,6 @@ function HW9() {
   return (
     <div className="homework">
       <h2 className="section_title">Homework 9</h2>
-      homeworks 9{/*should work (должно работать)*/}
       <Clock />
       <hr />
     </div>
