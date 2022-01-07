62cc842c421b9fdcfb23bcc37ed3d22c98e6b8a8
mykolao <mykolaoleskiv@gmail.com>
2022-01-07 13:15:47 +0200

    styling & animation completed

--- src/homework-10/HW10.tsx
+++ src/homework-10/HW10.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { CSSProperties } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import SuperButton from "../homework-4/common/c2-SuperButton/SuperButton";
 import { State, toggleLoading } from "./bll/loadingReducer";
+
+const Spinner = () => {
+  const [rotationAngle, setRotationAngle] = React.useState(1);
+
+  React.useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      console.log("Spinnin");
+      setRotationAngle(
+        (previous) =>
+          previous + Math.abs(Math.sin(previous)) * 25
+      );
+    }, 16);
+
+    return () => window.clearInterval(intervalId);
+  }, []);
+
+  const componentStyles: CSSProperties = {
+    paddingBottom: "12px",
+  };
+  const outerStyles: CSSProperties = {
+    height: "31px",
+    width: "54px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  const innerStyles: CSSProperties = {
+    width: "3px",
+    height: "27px",
+    backgroundColor: "var(--color-red)",
+    borderRadius: "50px",
+    transform: `rotate(${rotationAngle}deg)`,
+  };
+  return (
+    <div style={componentStyles}>
+      <div style={outerStyles}>
+        <div style={innerStyles}></div>
+      </div>
+    </div>
+  );
+};
 
 function HW10() {
   const dispatch = useDispatch();
@@ -14,18 +56,22 @@ function HW10() {
     window.setTimeout(dispatch, 2000, toggleLoading());
   };
 
+  const componentStyles: CSSProperties = {
+    margin: "12px",
+  };
+
   return (
     <div className="homework">
       <h2 className="section_title">Homework 10</h2>
-      {loading ? (
-        <div>крутилка...</div>
-      ) : (
-        <div>
-          <SuperButton onClick={setLoading}>
-            Load
-          </SuperButton>
-        </div>
-      )}
+      <div style={componentStyles}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div>
+            <SuperButton onClick={setLoading}>Load</SuperButton>
+          </div>
+        )}
+      </div>
       <hr />
     </div>
   );
