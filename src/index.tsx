6c94f9dfc4e99985134e4f8c85a1218270949235
mykolao <mykolaoleskiv@gmail.com>
2022-01-07 12:21:15 +0200

    store provided

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import HW5 from "./homework-5/HW5";
+import { Provider } from "react-redux";
+import store from "./homework-10/bll/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <HW5 />
+      <Provider store={store}>
+        <HW5 />
+      </Provider>
     </div>
   </React.StrictMode>,
   document.getElementById("root")
