cf915235e106d8341910cd944d0e346a31b3cf33
mykolao <mykolaoleskiv@gmail.com>
2022-01-10 04:46:50 +0200

    homework 11 init

---
+++ src/homework-11/HW11.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import SuperRange from "./common/c7-SuperRange/SuperRange";
+import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
+
+function HW11() {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(100);
+
+  return (
+    <div className="homework">
+      <div className="section_title">Homework 11</div>
+
+      {/*should work (должно работать)*/}
+      <div>
+        <span>{value1}</span>
+        <SuperRange
+        // сделать так чтоб value1 изменялось
+        />
+      </div>
+      <div>
+        <span>{value1}</span>
+        <SuperDoubleRange
+        // сделать так чтоб value1 и value2 изменялось
+        />
+        <span>{value2}</span>
+      </div>
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr />
+    </div>
+  );
+}
+
+export default HW11;
--- src/homework-5/pages/Junior.tsx
+++ src/homework-5/pages/Junior.tsx
@@ -1,4 +1,5 @@
 import HW10 from "../../homework-10/HW10";
+import HW11 from "../../homework-11/HW11";
 import HW7 from "../../homework-7/HW7";
 import HW8 from "../../homework-8/HW8";
 import HW9 from "../../homework-9/HW9";
@@ -11,6 +12,7 @@ const Junior = () => {
       <HW8 />
       <HW9 />
       <HW10 />
+      <HW11 />
     </>
   );
 };
