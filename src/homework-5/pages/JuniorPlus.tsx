3e5be141f7f88b2431ae0c2b8a79ce67d90e365d
mykolao <mykolaoleskiv@gmail.com>
2022-01-19 21:37:37 +0200

    homework 12 init

--- src/homework-5/pages/JuniorPlus.tsx
+++ src/homework-5/pages/JuniorPlus.tsx
@@ -1,5 +1,12 @@
+import HW12 from "../../homework-12/HW12";
+
 const JuniorPlus = () => {
-  return <div className="section_title">Junior+ Page</div>;
+  return (
+    <>
+      {/* <h2 className="section_title">Junior+ Page</h2>; */}
+      <HW12 />
+    </>
+  );
 };
 
 export default JuniorPlus;
