e1fc0797847daa9408584163e00f6893cbb03895
mykolao <mykolaoleskiv@gmail.com>
2022-01-19 22:51:41 +0200

    checkbox state completed

--- src/homework-13/components/Request.tsx
+++ src/homework-13/components/Request.tsx
@@ -1,11 +1,23 @@
+import React from "react";
 import SuperButton from "../../homework-4/common/c2-SuperButton/SuperButton";
 import SuperCheckbox from "../../homework-4/common/c3-SuperCheckbox/SuperCheckbox";
 
 const Request = () => {
+  const [checked, setChecked] = React.useState(false);
+
+  const onChecboxChange = () => {
+    setChecked((checked) => !checked);
+  };
+
   return (
     <>
       <SuperButton>Button</SuperButton>
-      <SuperCheckbox />
+      <SuperCheckbox
+        checked={checked}
+        onChange={onChecboxChange}
+      >
+        Success
+      </SuperCheckbox>
     </>
   );
 };
